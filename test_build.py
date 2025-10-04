634c494133f48939ac8cf8044732cfed21f874d0
AppSolves <appsolves@gmail.com>
2025-10-04 19:12:53 +0200

    Add more debugging to `test_build.py`

--- test_build.py
+++ test_build.py
@@ -3,7 +3,9 @@ import sys
 
 try:
     from pylocalauth import __version__
-except:
+except Exception as e:
+    print(f"Import failed: {e}")
     sys.exit(1)
 
+print(f"`pylocalauth` version: {__version__}")
 sys.exit(0)
